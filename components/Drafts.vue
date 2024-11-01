f6fd9a0c9903b1ecafcbbd4e0639470afb4b4122
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2024-11-01 23:28:57 +0300

    feat: added description to projects page

--- components/Drafts.vue
+++ components/Drafts.vue
@@ -7,7 +7,10 @@ const drafts = inject('drafts') as Draft[]
 
 let disableAllHandlers = false
 
+let activeMoving = ref<boolean>(false)
+
 function draftMoveHandler(e: MouseEvent) {
+  activeMoving.value = true
   const allDrafts = document.querySelectorAll<HTMLElement>('.drafts .drafts__item')
 
   const mouseX = e.clientX
@@ -58,6 +61,7 @@ function draftDownHandler(e: MouseEvent) {
 
 <template>
   <div class="drafts" @mousemove="draftMoveHandler" @mouseleave="draftLeaveHandler" @wheel="draftWheelhandler" @mousedown="draftDownHandler">
+    <div class="drafts__description" :class="{ active: activeMoving }">Some of my <b>projects</b>, that i can show to you, hermano:</div>
     <img :src="draft.image" :data-link="`/project/${draft.title}`" :style="`--speed: ${1 - i / 10}s`" alt="project image" class="drafts__item" v-for="draft, i in drafts" :key="draft.image" >
   </div>
 </template>
@@ -69,6 +73,27 @@ function draftDownHandler(e: MouseEvent) {
   left: 0;
   height: 100vh;
   width: 100%;
+
+  &__description {
+    visibility: hidden;
+    opacity: 0;
+    transform: translateY(1rem);
+    position: fixed;
+    top: 50%;
+    left: 70%;
+    max-width: 400px;
+    transition: 1s ease-out;
+
+    b {
+      font-weight: 700
+    }
+
+    &.active {
+      opacity: 1;
+      visibility: visible;
+      transform: translateY(0);
+    }
+  }
 
   &__item {
     filter: grayscale(1);
