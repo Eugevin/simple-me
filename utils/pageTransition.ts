6e20710e67c930902e24db82c7d034161133daf4
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2024-11-15 05:02:17 +0300

    feat: new page transition

--- utils/pageTransition.ts
+++ utils/pageTransition.ts
@@ -8,11 +8,12 @@ const meta: PageMeta = {
     mode: 'out-in',
     onEnter: async (_el, done) => {
       document.body.style.overflow = 'visible'
+      animate('#transition h3', { transform: ['translateY(0)', 'translateY(-5rem)'], opacity: [1, 0] }, { duration: 1, easing: easeInOutExpo })
       await animate('#transition', { clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)' }, { duration: 1, easing: easeInOutExpo }).finished
       done()
     },
     onLeave: async (_el, done) => {
-      animate('#transition h3', { scale: [1, 1.25] }, { duration: 3, easing: 'linear' })
+      animate('#transition h3', { transform: ['translateY(5rem)', 'translateY(0)'], opacity: [0, 1] }, { duration: 1, easing: easeInOutExpo })
       await animate('#transition', { visibility: 'visible', clipPath: ['polygon(0 0, 0 0, 0 100%, 0% 100%)', 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'] }, { duration: 1, easing: easeInOutExpo }).finished
       done()
     },
