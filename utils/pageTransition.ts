493d9276124687f251d118654e0c357a4387ba89
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2025-07-03 00:06:51 +0300

    feat: new max-len eslint rule

--- utils/pageTransition.ts
+++ utils/pageTransition.ts
@@ -1,6 +1,8 @@
 import { animate } from 'motion'
-import type { PageMeta } from '#app'
+
 import { easeInOutExpo } from '~/static/easings'
+
+import type { PageMeta } from '#app'
 
 const meta: PageMeta = {
   pageTransition: {
@@ -8,13 +10,47 @@ const meta: PageMeta = {
     mode: 'out-in',
     onEnter: async (_el, done) => {
       document.body.style.overflow = 'visible'
-      animate('#transition h3', { transform: ['translateY(0)', 'translateY(-5rem)'], opacity: [1, 0] }, { duration: 1, easing: easeInOutExpo })
-      await animate('#transition', { clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)' }, { duration: 1, easing: easeInOutExpo }).finished
+      animate('#transition h3',
+        {
+          transform: ['translateY(0)', 'translateY(-5rem)'],
+          opacity: [1, 0],
+        },
+        {
+          duration: 1,
+          easing: easeInOutExpo,
+        })
+      await animate('#transition',
+        {
+          clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
+        },
+        {
+          duration: 1,
+          easing: easeInOutExpo,
+        }).finished
       done()
     },
     onLeave: async (_el, done) => {
-      animate('#transition h3', { transform: ['translateY(5rem)', 'translateY(0)'], opacity: [0, 1] }, { duration: 1, easing: easeInOutExpo })
-      await animate('#transition', { visibility: 'visible', clipPath: ['polygon(0 0, 0 0, 0 100%, 0% 100%)', 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'] }, { duration: 1, easing: easeInOutExpo }).finished
+      animate('#transition h3',
+        {
+          transform: ['translateY(5rem)', 'translateY(0)'],
+          opacity: [0, 1],
+        },
+        {
+          duration: 1,
+          easing: easeInOutExpo,
+        })
+      await animate('#transition',
+        {
+          visibility: 'visible',
+          clipPath: [
+            'polygon(0 0, 0 0, 0 100%, 0% 100%)',
+            'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+          ],
+        },
+        {
+          duration: 1,
+          easing: easeInOutExpo,
+        }).finished
       done()
     },
     onAfterLeave() {
--- utils/throttle.ts
+++ utils/throttle.ts
@@ -1,14 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export function throttle(fn: Function, ms: number = 500) {
+export function throttle<T extends unknown[]>(
+  fn: (...args: T) => void, ms: number = 500,
+) {
   let throttle: boolean = false
 
-  return (...args: unknown[]) => {
+  return (...args: T) => {
     if (throttle) return
     throttle = true
     setTimeout(() => {
       throttle = false
     }, ms)
 
-    fn.call(fn, ...args)
+    fn(...args)
   }
 }
